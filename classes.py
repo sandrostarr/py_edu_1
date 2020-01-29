05831d0fd092ad64630ceffc79151b39c668c658
SBesedin <sbesedin@gorvodokanal.com>
2020-01-29 16:46:10 +0700

    lesson36 and lesson37

---
+++ classes.py
@@ -0,0 +1,7 @@
+class Person:
+
+    def __init__(self, name):
+        self.name = name
+
+    def print_info(self):
+        print(f'Hello, my name is {self.name}')
---
+++ lesson37.py
@@ -0,0 +1,8 @@
+from classes import Person
+# import classes
+
+person1 = Person('John')
+person1.print_info()
+
+person2 = Person('Katy')
+person2.print_info()
