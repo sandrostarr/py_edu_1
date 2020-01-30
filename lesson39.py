c796dba6fdc2487cbda85692ca500035d98b8bc2
SBesedin <sbesedin@gorvodokanal.com>
2020-01-30 16:36:44 +0700

    lesson39

---
+++ lesson39.py
@@ -0,0 +1,7 @@
+from Parser_class import Parser
+
+parser = Parser('https://www.ua-football.com/sport', 'news.txt')
+parser.run()
+# print(parser.raw_html)
+# print(parser.html) 
+# print(parser.results)
