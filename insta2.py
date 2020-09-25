24c78ef249fd927471061a042615f89da69be858
SBesedin <sbesedin@gorvodokanal.com>
2020-09-25 10:59:07 +0700

    upd insta

--- insta2.py
+++ insta2.py
@@ -1,18 +1,21 @@
 from instapy import InstaPy
 
-session = InstaPy(username="sandrostarr.art", password="5andr5wagm0ney")
+session = InstaPy(username="sandrostarr.art", password="5andr5wagm0neyone")
 session.login()
 session.set_delimit_liking(enabled=True, max_likes=200, min_likes=0)
-session.set_action_delays(enabled=True, like=1, randomize=True, random_range_from=75, random_range_to=125)
-session.set_do_story(enabled = True, percentage = 70, simulate = False)
-session.set_relationship_bounds(enabled=True,
+# session.set_action_delays(enabled=True, like=15, randomize=True, random_range_from=75, random_range_to=125)
+session.set_action_delays(enabled=True, like=0.15, safety_match=False)
 
-				  delimit_by_numbers=True,
-				   max_followers=500,
-				    max_following=1000,
-				       min_posts=5,
-)
-session.like_by_tags(["abstractart", "abstractobsession", "artistsoninstagram", "abstractartwork", "primitivism"], amount=19) # [1]
+# session.set_relationship_bounds(enabled=True,
+
+# 				  delimit_by_numbers=True,
+# 				   max_followers=500,
+# 				    max_following=1000,
+# 				       min_posts=5,
+# )
+# session.set_user_interact(amount=1, randomize=True, percentage=15, media='Photo')
+# session.set_do_story(enabled = True, percentage = 60, simulate = False)
+session.like_by_tags(["abstractart", "abstractobsession", "artistsoninstagram", "abstractartwork", "primitivism"], amount=21) # [1]
 
 
 # session.set_user_interact(amount=2,
