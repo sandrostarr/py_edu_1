25f5d82c019f0052be6fbfdbcda00604a2d1f302
SBesedin <sbesedin@gorvodokanal.com>
2020-09-28 12:28:52 +0700

    new bot

--- insta.py
+++ insta.py
@@ -1,5 +1,7 @@
+import random
 from time import sleep
 from selenium import webdriver
+
 
 browser = webdriver.Firefox()
 browser.get('https://www.instagram.com/')
@@ -9,12 +11,39 @@ sleep(5)
 username_input = browser.find_element_by_css_selector("input[name='username']")
 password_input = browser.find_element_by_css_selector("input[name='password']")
 
-username_input.send_keys("<имя пользователя>")
-password_input.send_keys("<пароль>")
+username_input.send_keys("sandrostarr.art")
+password_input.send_keys("5andr5wagm0ney0ne")
 
 login_button = browser.find_element_by_xpath("//button[@type='submit']")
 login_button.click()
 
-sleep(15)
+sleep(8)
 
-browser.close()
+# search_input = browser.find_element_by_class_name("x3qfX")
+# search_input.send_keys("#abstractart")
+# search_input.submit()
+
+browser.get('https://www.instagram.com/explore/tags/abstractart/')
+
+sleep(8)
+
+
+browser.find_elements_by_class_name('v1Nh3')[9].click() # click on photo to open and upload
+
+
+item = 1
+likes = 10
+while item <= likes: # loop with how many photos to like
+
+    sleep(random.uniform(1, 6))
+    browser.find_element_by_class_name('fr66n').click() # click the like button
+    sleep(random.uniform(1, 3))
+    browser.find_element_by_class_name('coreSpriteRightPaginationArrow').click() # click on next photo button
+    item = item + 1
+
+print(f'Number of photos liked: \033[0;33m{item - 1}\033[m')
+
+# sleep(15)
+
+# browser.close()    
+# 
--- insta2.py
+++ insta2.py
@@ -1,10 +1,15 @@
 from instapy import InstaPy
 
-session = InstaPy(username="sandrostarr.art", password="5andr5wagm0neyone")
+session = InstaPy(username="sandrostarr.art", password="5andr5wagm0ney0ne")
 session.login()
 session.set_delimit_liking(enabled=True, max_likes=200, min_likes=0)
-# session.set_action_delays(enabled=True, like=15, randomize=True, random_range_from=75, random_range_to=125)
-session.set_action_delays(enabled=True, like=0.15, safety_match=False)
+session.set_action_delays(enabled=True, like=15, randomize=True, random_range_from=55, random_range_to=165)
+session.set_quota_supervisor(enabled=True, sleep_after=["likes", "server_calls"], sleepyhead=True, notify_me=True,
+                                peak_likes_hourly=20,
+                                peak_likes_daily=200,
+                                peak_server_calls_hourly=150,
+                                peak_server_calls_daily=1500)
+# session.set_action_delays(enabled=True, like=0.15, safety_match=False)
 
 # session.set_relationship_bounds(enabled=True,
 
@@ -15,7 +20,7 @@ session.set_action_delays(enabled=True, like=0.15, safety_match=False)
 # )
 # session.set_user_interact(amount=1, randomize=True, percentage=15, media='Photo')
 # session.set_do_story(enabled = True, percentage = 60, simulate = False)
-session.like_by_tags(["abstractart", "abstractobsession", "artistsoninstagram", "abstractartwork", "primitivism"], amount=21) # [1]
+session.like_by_tags(["abstractart", "abstractobsession", "artistsoninstagram", "abstractartwork", "primitivism"], amount=2) # [1]
 
 
 # session.set_user_interact(amount=2,
